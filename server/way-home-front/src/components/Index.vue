bbe1277d4d4308b01322b62394280cdcd74dc7b4
zhangtory <zhangtory@users.noreply.github.com>
2020-01-03 21:22:52 +0800

    add index

--- server/way-home-front/src/components/Index.vue
+++ server/way-home-front/src/components/Index.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="index">
-    <h1>{{msg}}</h1>
-    <router-link to="/register">马上注册</router-link>
-    <router-link to="/login">马上登录</router-link>
-    <router-link to="/dashboard">个人中心</router-link>
+    <Row type="flex" justify="center">
+      <Col>
+        <h1>WayHome 回家之路</h1>
+
+        <div class="detail">
+          <p>对浏览器支持的协议，它比DDNS更方便。</p>
+          <p>如果你在家搭建了NAS和HomeAssistant，那么WayHome是你最佳的选择。</p>
+          <p>无需注册域名，无需自建服务器，一个书签即可踏上回家之路。</p>
+        </div>
+
+        <div>
+          <router-link to="/login"><Button type="success" size="large" ghost>登录</Button></router-link>
+          <router-link to="/register"><Button type="info" size="large" ghost>注册</Button></router-link>
+        </div>
+
+        <div class="link">
+          <a href="https://github.com/zhangtory/WayHome" target="view_window">View On GitHub</a>
+        </div>
+      </Col>
+    </Row>
   </div>
 </template>
 
@@ -19,5 +35,39 @@
 </script>
 
 <style scoped>
+  h1 {
+    font-size: 5em;
+    color: #e6e6e6;
+    margin-bottom: 1em;
+  }
+  p {
+    font-size: 2em;
+    color: #e6e6e6;
+    margin-bottom: 0.3em;
+  }
+  Button {
+    width: 20%;
+  }
+  a {
+    font-size: 1.2em;
+  }
+  .link {
+    margin-top: 2em;
+  }
+  .detail {
+    margin-bottom: 4em;
+  }
+  .index {
+    width: 100vw;
+    height: 100vh;
+    background-image: url("../../static/stars_bg.jpg");
+    background-size: cover;
+    background-position: center;
 
+    text-align: center;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
--- server/way-home-front/src/router/index.js
+++ server/way-home-front/src/router/index.js
@@ -15,7 +15,7 @@ export default new Router({
     {
       path: '/',
       name: 'Index',
-      component: Login
+      component: Index
     },
     {
       path: '/register',
