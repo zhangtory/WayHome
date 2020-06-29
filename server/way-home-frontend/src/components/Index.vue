751064e1736bdb83070a01f22d2a7c360fc8ab9c
zhangtory <zhangtory@users.noreply.github.com>
2020-06-29 21:00:14 +0800

    创建项目框架

---
+++ server/way-home-frontend/src/components/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="index">
+      INDEX
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Index"
+    }
+</script>
+
+<style scoped>
+  .index {
+    width: 100%;
+    height: 100%;
+    background-image: url("../../static/stars_bg.jpg");
+    background-size: cover;
+    background-position: center;
+
+    text-align: center;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
---
+++ server/way-home-frontend/src/components/KeyList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>KEY LIST</div>
+</template>
+
+<script>
+    export default {
+        name: "KeyList"
+    }
+</script>
+
+<style scoped>
+
+</style>
